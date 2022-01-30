34a0b9731bbcb315f9f3d3f6825d34b6fda976f4
Jarosław Bagnicki <jaroslaw.bagnicki@gmail.com>
2022-01-30 12:01:12 +0100

    Add lazy loading of configuration

--- course-02/exercises/udacity-c2-restapi/src/config/configurationProvider.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/configurationProvider.ts
@@ -2,6 +2,7 @@ import AWS = require('aws-sdk');
 
 const ssm = new AWS.SSM();
 const keyPrefix = `/${process.env.APP_NAME}/${process.env.STAGE}/${process.env.SERVICE_NAME}`;
+let configuration: UdagramConfiguration = null;
 
 const getValueFromSsm = async (key: string): Promise<string> => ssm
   .getParameter({ Name: keyPrefix + key })
@@ -23,20 +24,42 @@ const getValueFromSsm = async (key: string): Promise<string> => ssm
   });
 
 export const getConfiguration = async () => {
-  return {
-    'aws_profile': process.env.AWS_PROFILE,
-    'db': {
-      'host': await getValueFromSsm('/db/host'),
-      'database': await getValueFromSsm('/db/dbname'),
-      'username': await getValueFromSsm('/db/username'),
-      'password': await getValueFromSsm('/db/password'),
-    },
-    'filestore': {
-      'aws_media_bucket': await getValueFromSsm('/filestore/bucket'),
-      'aws_region': process.env.AWS_REGION,
-    },
-    'jwt': {
-      'secret': await getValueFromSsm('/jwt/secret'),
-    }
-  };
+  if (!configuration) {
+    configuration = {
+      'aws_profile': process.env.AWS_PROFILE,
+      'db': {
+        'host': await getValueFromSsm('/db/host'),
+        'database': await getValueFromSsm('/db/dbname'),
+        'username': await getValueFromSsm('/db/username'),
+        'password': await getValueFromSsm('/db/password'),
+      },
+      'filestore': {
+        'aws_media_bucket': await getValueFromSsm('/filestore/bucket'),
+        'aws_region': process.env.AWS_REGION,
+      },
+      'jwt': {
+        'secret': await getValueFromSsm('/jwt/secret'),
+      }
+    };
+  }
+  return configuration;
 };
+
+// tslint:disable-next-line: interface-over-type-literal
+type UdagramConfiguration = {
+  'aws_profile': string,
+  'db': {
+    'host': string,
+    'database': string,
+    'username': string,
+    'password': string,
+  },
+  'filestore': {
+    'aws_media_bucket': string,
+    'aws_region': string
+  },
+  'jwt': {
+    'secret': string,
+  }
+};
+
